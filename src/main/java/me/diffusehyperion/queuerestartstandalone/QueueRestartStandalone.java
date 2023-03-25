9b657e0dd36935513db552465c5159dc20969295
DiffuseHyperion <xieyingjie08@gmail.com>
2023-03-25 14:56:34 +0800

    Initial commit

---
+++ src/main/java/me/diffusehyperion/queuerestartstandalone/QueueRestartStandalone.java
@@ -0,0 +1,17 @@
+package me.diffusehyperion.queuerestartstandalone;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class QueueRestartStandalone extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
